81c721f0a456ad390a37efe44381bb554d96f49b
Daniel Oshiro <dobrado@tutanota.com>
2021-09-19 19:37:35 +0100

    feat: split components and add container

---
+++ components/Section.vue
@@ -0,0 +1,5 @@
+<template>
+  <section>
+    Section
+  </section>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,24 @@
 <template>
-  <Tutorial/>
+  <main class="container">
+    <Sidebar />
+    <Section />
+  </main>
 </template>
 
 <script>
-export default {}
+import Sidebar from '~/components/Sidebar'
+import Section from '~/components/Section'
+
+export default {
+  components: {
+    Sidebar,
+    Section,
+  }
+}
 </script>
+
+<style lang="postcss">
+.container {
+  @apply mx-auto;
+}
+</style>
