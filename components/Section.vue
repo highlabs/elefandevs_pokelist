7bf59bd574cfe89d8c17745a852f18e5aac94005
Dandelião <danielramos@tuta.io>
2021-09-20 23:34:09 -0300

    feat: adiciona botão "carregar mais"

--- components/Section.vue
+++ components/Section.vue
@@ -10,6 +10,7 @@
         />
       </li>
     </ul>
+    <button @click="$emit('carregar-mais')">Carregar mais</button>
   </section>
 </template>
 
@@ -33,5 +34,8 @@ export default {
 <style lang="postcss" scoped>
 section {
   @apply w-full px-4;
+}
+button {
+  @apply w-full py-4 bg-gray-500 mb-32 rounded-lg;
 }
 </style>
